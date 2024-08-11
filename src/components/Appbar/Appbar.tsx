e1df4fc152044e90ee0141edb17f3f807f7119ce
Shubham Shinde <shubhamshinde225@gmail.com>
2024-08-12 00:45:48 +0530

    UI for appbar and sidebar created

--- src/components/Appbar/Appbar.tsx
+++ src/components/Appbar/Appbar.tsx
@@ -1,15 +1,31 @@
-import { AppBar, Box, Button, Divider, MenuItem, SvgIcon, Toolbar, Typography } from '@mui/material';
-import {ReactComponent as AppLogo} from './app-logo.svg';
+import { AppBar, Avatar, Box, Button, ButtonGroup, Divider, ListItemIcon, Menu, MenuItem, Toolbar, Typography } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import { useNavigate } from 'react-router-dom';
+import { Logout, PersonAdd } from '@mui/icons-material';
+import KeyIcon from '@mui/icons-material/Key';
+import React from 'react';
 
 const logoStyle = {
-  width: '140px',
+  width: '160px',
   height: 'auto',
   cursor: 'pointer',
+  marginLeft: '10px'
 };
 
 const drawerWidth = 240;
 
 export const Appbar = () => {
+  const navigateTo = useNavigate();
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <AppBar
         position="fixed"
@@ -40,19 +56,6 @@ export const Appbar = () => {
                 : '0 0 1px rgba(2, 31, 59, 0.7), 1px 1.5px 2px -1px rgba(2, 31, 59, 0.65), 4px 4px 12px -2.5px rgba(2, 31, 59, 0.65)',
           })}
           >
-          {/* <Box
-            sx={{
-              flexGrow: 1,
-              display: 'flex',
-              alignItems: 'center',
-              ml: '-18px',
-              px: 0,
-            }}
-          >
-          <Typography variant="h6" noWrap component="div">
-            Permanent drawer
-          </Typography>
-          </Box> */}
           <Box
               sx={{
                 flexGrow: 1,
@@ -66,17 +69,12 @@ export const Appbar = () => {
                 width: `${drawerWidth}px`,}}>
                 <img
                   src={
-                    'https://assets-global.website-files.com/61ed56ae9da9fd7e0ef0a967/61f12e6faf73568658154dae_SitemarkDefault.svg'
+                    './imgs/app-logo.svg'
                   }
                   style={logoStyle}
                   alt="logo of sitemark"
+                  onClick={(e) => navigateTo('/home')}
                 />
-                {/* <SvgIcon sx={{
-              height: '-webkit-fill-available',
-              width: '-webkit-fill-available'
-            }}>
-                  <AppLogo />
-                </SvgIcon> */}
               </Box>
               <Box sx={{ display: { xs: 'none', md: 'flex' } }}>
                   <Typography variant="h6" color="text.primary">
@@ -91,17 +89,76 @@ export const Appbar = () => {
                 alignItems: 'center',
               }}
             >
-              <Button
-                color="primary"
-                variant="outlined"
-                size="small"
-                component="a"
-                href="/material-ui/getting-started/templates/sign-in/"
-                target="_blank"
-              >
-                Admin
-              </Button>
+              <ButtonGroup color="primary" aria-label="Medium-sized button group">
+                <Button
+                  color="primary"
+                  variant="outlined"
+                  size="medium"
+                >
+                  Admin
+                </Button>
+                <Button
+                  color="primary"
+                  variant="outlined"
+                  size="small"
+                  onClick={handleClick}
+                >
+                  <PersonIcon />
+                </Button>
+              </ButtonGroup>
             </Box>
+            <Menu
+              anchorEl={anchorEl}
+              id="account-menu"
+              open={open}
+              onClose={handleClose}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: 'visible',
+                  filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                  mt: 1.5,
+                  '& .MuiAvatar-root': {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  '&::before': {
+                    content: '""',
+                    display: 'block',
+                    position: 'absolute',
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: 'background.paper',
+                    transform: 'translateY(-50%) rotate(45deg)',
+                    zIndex: 0,
+                  },
+                },
+              }}
+              transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+              anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+            >
+              <MenuItem onClick={handleClose}>
+                <Avatar /> My account
+              </MenuItem>
+              <Divider />
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <KeyIcon fontSize="small" />
+                </ListItemIcon>
+                Change Password
+              </MenuItem>
+              <MenuItem onClick={handleClose}>
+                <ListItemIcon>
+                  <Logout fontSize="small" />
+                </ListItemIcon>
+                Logout
+              </MenuItem>
+            </Menu>
         </Toolbar>
       </AppBar>
   )
--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,11 +1,40 @@
-import { Box, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import { Collapse, Divider, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar } from '@mui/material'
+import HomeIcon from '@mui/icons-material/Home';
+import GppGoodIcon from '@mui/icons-material/GppGood';
+import SecurityIcon from '@mui/icons-material/Security';
+import NotificationsActiveIcon from '@mui/icons-material/NotificationsActive';
+import TerminalIcon from '@mui/icons-material/Terminal';
+import DataObjectIcon from '@mui/icons-material/DataObject';
+import ManageAccountsIcon from '@mui/icons-material/ManageAccounts';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import GroupsIcon from '@mui/icons-material/Groups';
+import AdminPanelSettingsIcon from '@mui/icons-material/AdminPanelSettings';
+import { useNavigate } from 'react-router-dom';
+import { ExpandLess, ExpandMore } from '@mui/icons-material';
+import React from 'react';
 
 const drawerWidth = 240;
 const appbarHeight = 64;
 
 export const Sidebar = () => {
+  const navigateTo = useNavigate();
+  const [openApp, setOpenApp] = React.useState(false);
+  const [openClient, setOpenClient] = React.useState(false);
+  const [openUser, setOpenUser] = React.useState(false);
+
+  const handleAppOpen = () => {
+    setOpenApp(!openApp);
+  }
+
+  const handleOpenClient = () => {
+    setOpenClient(!openClient);
+  }
+
+  const handleOpenUser = () => {
+    setOpenUser(!openUser);
+  }
+
   return (
     <Drawer
         sx={{
@@ -22,29 +51,115 @@ export const Sidebar = () => {
       >
         <Divider />
         <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
+            <ListItem key='home' disablePadding>
+              <ListItemButton onClick={(e) => navigateTo('/home')}>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <HomeIcon/>
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary='Home' />
               </ListItemButton>
             </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem key={text} disablePadding>
-              <ListItemButton>
+
+            <ListItemButton onClick={handleAppOpen}>
+              <ListItemIcon>
+                <TerminalIcon />
+              </ListItemIcon>
+              <ListItemText primary="Application" />
+              {openApp ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openApp} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItemButton 
+                  sx={{ pl: 4 }} 
+                  onClick={(e) => navigateTo('/auth')}>
+                    <ListItemIcon>
+                      <GppGoodIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Authentication" />
+                </ListItemButton>
+                <ListItemButton 
+                  sx={{ pl: 4 }} 
+                  onClick={(e) => navigateTo('/security')}>
+                  <ListItemIcon>
+                    <SecurityIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Security" />
+                </ListItemButton>
+                <ListItemButton 
+                  sx={{ pl: 4 }} 
+                  onClick={(e) => navigateTo('/alert')}>
+                  <ListItemIcon>
+                    <NotificationsActiveIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Alert" />
+                </ListItemButton>
+              </List>
+            </Collapse>
+
+            <ListItem key='jwt' disablePadding>
+              <ListItemButton onClick={(e) => navigateTo('/jwt')}>
                 <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                  <DataObjectIcon/>
                 </ListItemIcon>
-                <ListItemText primary={text} />
+                <ListItemText primary='JWT Config' />
               </ListItemButton>
             </ListItem>
-          ))}
+
+            <ListItemButton onClick={handleOpenClient}>
+              <ListItemIcon>
+                <PeopleAltIcon />
+              </ListItemIcon>
+              <ListItemText primary="Client Management" />
+              {openApp ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openClient} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItemButton 
+                  sx={{ pl: 4 }} 
+                  onClick={(e) => navigateTo('/scope')}>
+                    <ListItemIcon>
+                      <AdminPanelSettingsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Scope" />
+                </ListItemButton>
+                <ListItemButton 
+                  sx={{ pl: 4 }} 
+                  onClick={(e) => navigateTo('/client')}>
+                  <ListItemIcon>
+                    <GroupsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="Client" />
+                </ListItemButton>
+              </List>
+            </Collapse>
+
+            <ListItemButton onClick={handleOpenUser}>
+              <ListItemIcon>
+                <AssignmentIndIcon />
+              </ListItemIcon>
+              <ListItemText primary="User Management" />
+              {openApp ? <ExpandLess /> : <ExpandMore />}
+            </ListItemButton>
+            <Collapse in={openUser} timeout="auto" unmountOnExit>
+              <List component="div" disablePadding>
+                <ListItemButton 
+                  sx={{ pl: 4 }} 
+                  onClick={(e) => navigateTo('/role')}>
+                    <ListItemIcon>
+                      <AdminPanelSettingsIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Role" />
+                </ListItemButton>
+                <ListItemButton 
+                  sx={{ pl: 4 }} 
+                  onClick={(e) => navigateTo('/user')}>
+                  <ListItemIcon>
+                    <ManageAccountsIcon />
+                  </ListItemIcon>
+                  <ListItemText primary="User" />
+                </ListItemButton>
+              </List>
+            </Collapse>
         </List>
       </Drawer>
   )
